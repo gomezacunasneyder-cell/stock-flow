432542bb2884c54e16ab0fd1c60a416b0697829d
Michael Vega <supermavp@gmail.com>
2026-06-24 20:09:57 -0500

    feat: Adding items to localStorage

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+items = [];
+
 document.addEventListener("product-submit", (event) => {
-  console.log("Producto guardado:", event.detail);
-  
+  const item = event.detail;
+  items.push(item);
+  localStorage.setItem("items", JSON.stringify(items));
 });
